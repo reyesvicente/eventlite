5299a0d84fec65be15f05a090dd77efeb29dd737
Vicente G. Reyes <icenreyes@icloud.com>
2020-07-14 00:47:24 +0800

    end of 5.5

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
-	validates_presence_of :title, :start_datetime, :location
+  validates_presence_of :title, :start_datetime, :location
+  validates :title, length: {minimum: 3}
+  validate :start_datetime_cannot_be_in_the_past
+
+  belongs_to :user
+
+  private
+  def start_datetime_cannot_be_in_the_past
+    if start_datetime.present? && start_datetime < Time.now
+      errors.add(:start_datetime, "can't be in the past")
+    end
+  end
 end

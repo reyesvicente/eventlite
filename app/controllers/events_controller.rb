98efc01ea181e664937cd3523f4f86b51f04cafa
Vicente G. Reyes <icenreyes@icloud.com>
2020-07-21 01:29:58 +0800

    end of 6.7

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,8 +15,16 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
-    puts @event.as_json
-    render json: @event.as_json(except: :user_id, include: {user: {only: [:name, :nickname, :image]}})
+    render json: @event.as_json(except: :user_id, include: {user: {only: [:name, :nickname, :image]}}).merge(currentUserCanEdit: @event.user.email == request.headers['uid'])
+  end
+
+  def update
+    @event = current_user.events.find(params[:id])
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
   end
 
   private

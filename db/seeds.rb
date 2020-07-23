f970b4c8796741a79a0e83ce30f8de1406306485
Vicente G. Reyes <icenreyes@icloud.com>
2020-07-23 18:39:27 +0800

    start of react-bootstrap lesson

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-events = Event.create([
-  {
-    title: "London Retail Expo",
-    start_datetime: "Mon, 14 Oct 2019",
-    location: "London Excel Centre"
-  },
-  {
-    title: "Enterprise Sales Training Workshop",
-    start_datetime: "Tue, 15 Oct 2019",
-    location: "Expert Sales Company Headquarters"
-  },
-  {
-    title: "Ruby Hack Night",
-    start_datetime: "Fri, 18 Oct 2019",
-    location: "Learnetto Headquarters"
-  },
-  {
-    title: "Beginners Salsa dance meetup",
-    start_datetime: "Sat, 14 Oct 2019",
-    location: "Bar Salsa"
-  }
-])
+events = Event.create([{
+    "id": 25,
+    "title": "Cooking with Kelly",
+    "start_datetime": "2020-07-27T00:00:00.000Z",
+    "location": "San Francisco",
+    "created_at": "2020-07-22T12:35:37.089Z",
+    "updated_at": "2020-07-22T12:36:32.396Z",
+    "user_id": 1,
+    "description": "\u003cdiv class=\"structured-content-rich-text structured-content__module l-align-left l-mar-vert-6 l-sm-mar-vert-4 text-body-medium\"\u003e\n\t\u003cp\u003ePrepare yourself for a new career path as a personal chef. Whether part-time supplemental income, or as a full-time career path, being a personal chef is a rewarding career option with many different avenues to keep your creativity at its peak.\u003c/p\u003e\u003cp\u003eIn this  on-line course, you'll learn how to:\u003c/p\u003e\u003cul\u003e\u003cli\u003eSet Up a Website and Create Your Brand\u003c/li\u003e\u003cli\u003eUnderstand regulations and operate legally\u003c/li\u003e\u003cli\u003eFind Your First Client\u003c/li\u003e\u003cli\u003eNavigate the New Client Meeting\u003c/li\u003e\u003cli\u003ePrepare for your first cooking day\u003c/li\u003e\u003cli\u003eAsk for and receive valuable feedback\u003c/li\u003e\u003cli\u003eStreamline the Invoicing and Billing Process\u003c/li\u003e\u003c/ul\u003e\u003cp\u003eWe'll also cover a bonus content sections:\u003c/p\u003e\u003cul\u003e\u003cli\u003e Avoiding Common Personal Chef Pitfalls\u003c/li\u003e\u003cli\u003eProviding Personal Chef Service during Covid-19 \u003c/li\u003e\u003c/ul\u003e\u003cp\u003eEach section will allow time for Q\u0026amp;A so specific questions you have may be answered.\u003c/p\u003e\u003cp\u003eYou will also have access to downloadable content including a Client Preferences Worksheet, Kitchen Inventory Sheet, and a Sample Client contract. \u003c/p\u003e\u003cp\u003eIf you can't attend the entire live summit, you will receive access to watch the recording at a later time.\u003c/p\u003e\n\u003c/div\u003e",
+    "image_url": "https://images.unsplash.com/photo-1578738288760-05ce9be719d3?crop=entropy\u0026cs=tinysrgb\u0026fit=crop\u0026fm=jpg\u0026h=370\u0026ixid=eyJhcHBfaWQiOjF9\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=740"
+}, {
+    "id": 6,
+    "title": "Yoga with Tiffy Jameson",
+    "start_datetime": "2020-07-30T15:00:37.364Z",
+    "location": "Online",
+    "created_at": "2020-06-24T07:04:41.663Z",
+    "updated_at": "2020-07-22T15:23:01.352Z",
+    "user_id": 1,
+    "description": "\t\u003cp\u003eYoga helps us to care for the most important asset \nwe have our body.\n\u003c/p\u003e\u003cp\u003eThis gentle yoga flow will help to release tension \nand tightness and increase range of motion in the \nbody. In the class we will use breathing techniques \nand meditation to help lower stress levels and \ncalm a busy, over thinking mind. Techniques will \nbe taught that you can use at home in your own \npractice (or to start a practice.)\n\u003c/p\u003e\u003cp\u003e\nClasses are complete beginner friendly!\n\u003c/p\u003e\u003cp\u003e\nYou will need a mat or a soft surface to practice \non, and possibly a blanket or pillow to protect your knees or sit on. \n\u003c/p\u003e\u003cp\u003e\u003c/p\u003e\n",
+    "image_url": "https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?crop=entropy\u0026cs=tinysrgb\u0026fit=crop\u0026fm=jpg\u0026h=370\u0026ixid=eyJhcHBfaWQiOjF9\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=740"
+}]
+)
